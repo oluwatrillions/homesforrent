f355ce7b88e2a5136a3ad4a962120acf9a07422a
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2023-01-26 12:32:14 +0100

    ...

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
+
 const Layout = ({children}) => {
   return (
       <>
           <Header />
-          { children}
+          <main>{ children}</main>
           <Footer />
       </>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Explore from '../components/Explore'
 
